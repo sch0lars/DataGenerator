b0cfbd480972cc00c4f9c17681242ecef187e8d4
sch0lars <sch0lars@users.noreply.github.com>
2021-01-07 20:35:06 -0700

    Made various bug fixes, added a new format option, and modified the sample .xlsx file.

--- generator.py
+++ generator.py
@@ -1,19 +1,25 @@
+import argparse
 import openpyxl
 import random
 import re
 import string
 
 
-# TODO: Remove the test portion of the code.
+# Create an argument parser.
+parser = argparse.ArgumentParser(description='Generate random test data into SQL insert statements using formatted strings in an Excel file.')
+# Add arguments.
+parser.add_argument('-n', '--rows', metavar='rows', type=int, default=10, required=False, help='the number of insert statements to generate (default = 10)')
+# Parse the arguments.
+args = parser.parse_args()
+
 # Assume we have something like AX5[A-Z]{3}[0-9]{4}[A-Z]{2}[0-9]{1}
-##### Test #####
-# pattern = 'VX[A-Z]{3}[1-5]{5}'
-# print(pattern)
 
 # Load the first and last names.
-with open('first-names.txt') as file:
+first_names_txt = 'first-names.txt'
+last_names_txt = 'last-names.txt'
+with open(first_names_txt) as file:
     first_names = [name.strip() for name in file.read().split('\n')]
-with open('last-names.txt') as file:
+with open(last_names_txt) as file:
     last_names = [name.strip() for name in file.read().split('\n')]
 
 # Substitute the patterns.
@@ -24,62 +30,96 @@ def replace_lowercase(pattern: str) -> str:
         replacement = ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
         pattern = pattern.replace(sub_pattern, replacement, 1)
     return pattern
+
+
 # Lowercase letters.
 def replace_uppercase(pattern: str) -> str:
+    """Replace uppercase placeholder values with uppercase letters"""
     for sub_pattern in re.findall('\[A\-Z\]\{\d+\}', pattern):
         length = int(re.findall('(?<=\{)\d+(?=\})', sub_pattern)[0])
         replacement = ''.join(random.choice(string.ascii_uppercase) for _ in range(length))
         pattern = pattern.replace(sub_pattern, replacement, 1)
     return pattern
+
+
 # Digits
 def replace_digits(pattern: str) -> str:
+    """Replace integer placeholder values with integers"""
     for sub_pattern in re.findall('\[\d+\-\d+\]\{\d+\}', pattern):
         length = int(re.findall('(?<=\{)\d+(?=\})', sub_pattern)[0])
         start, end = [int(d) for d in re.findall('\d+\-\d+', sub_pattern)[0].split('-')]
         replacement = ''.join(str(random.randint(start, end)) for _ in range(length))
         pattern = pattern.replace(sub_pattern, replacement, 1)
     return pattern
+
+
 # Decimals.
 def replace_decimals(pattern: str) -> str:
-    for sub_pattern in re.findall('\[\d+\.\d+\-\d+\.\d+\]\{\d+\}', pattern):
-        length = int(re.findall('(?<=\{)\d+(?=\})', sub_pattern)[0])
-        start, end = [int(float(d)*100) for d in re.findall('\d+\.\d+\-\d+\.\d+', sub_pattern)[0].split('-')]
-        replacement = ''.join(str(random.randint(start, end)/100))
+    """Replace decimal placeholder values with decimals"""
+    for sub_pattern in re.findall('\[\d+\.\d+\-\d+\.\d+\]', pattern):
+        start, end = re.findall('\d+\.\d+', sub_pattern)
+        decimal_places = len(start.split('.')[1])
+        start = float(start)*pow(10, decimal_places)
+        end = float(end)*pow(10, decimal_places)
+        replacement = str(random.randrange(start, end)/pow(10, decimal_places))
         pattern = pattern.replace(sub_pattern, replacement, 1)
     return pattern
+
+# Alphanumeric
+def replace_alphanum(pattern: str) -> str:
+    """Replace alphanumeric placeholder values with alphanumeric values"""
+    for sub_pattern in re.findall('\{\{alphanum\}\}\{\d+\}', pattern):
+        length = int(re.findall('\d+', sub_pattern)[0])
+        replacement = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+        pattern = pattern.replace(sub_pattern, replacement)
+    return pattern
+
+
 # Lists of values
 def replace_lists(pattern: str) -> str:
+    """Replace list placeholder values with lists of values"""
     for sub_pattern in re.findall('\[[\w\d\s\,]+\]\{\d+\}', pattern):
         length = int(re.findall('(?<=\{)\d+(?=\})', sub_pattern)[0])
         value_list = [value.strip() for value in re.findall('(?<=\[)[\w\d\s\,]+(?=\])', sub_pattern)[0].split(',')]
         replacement = ''.join(random.choice(value_list) for _ in range(length))
         pattern = pattern.replace(sub_pattern, replacement)
     return pattern
+
+
 # Replace names.
-def replace_names(pattern: str) -> str:
-    first_name = random.choice(first_names)
-    last_name = random.choice(last_names)
+def replace_names(pattern: str, first_name: str, last_name) -> str:
+    """Replace name placeholder values with first and last names"""
     email = first_name + random.choice(['.', '_', '']) + last_name + '@' + random.choice(['example.com', 'example.net', 'test.com', 'test.net', 'mail.com', 'mail.net', 'gmail.com', 'yahoo.com', 'outlook.com'])
     primary_address = ''.join(random.choice(string.digits) for _ in range(random.randint(2, 4))) + ' ' + random.choice(last_names).upper() + ' ' + random.choice(['RD', 'LN', 'AVE', 'BLVD', 'ST', 'PL', 'WAY'])
-    secondary_address = random.choice(['APT ', ' STE' '#']) + ''.join(random.choice(string.digits) for _ in range(random.randint(1, 4)))
+    secondary_address = random.choice(['APT ', 'STE' '#']) + ''.join(random.choice(string.digits) for _ in range(random.randint(1, 4)))
     pattern = pattern.replace('{{first_name}}', first_name)
     pattern = pattern.replace('{{last_name}}', last_name)
+    pattern = pattern.replace('{{email}}', email)
     pattern = pattern.replace('{{primary_address}}', primary_address)
     pattern = pattern.replace('{{primary_address}}', secondary_address)
     return pattern
 
+
 ##### Loading the Excel file #####
 excel_file = 'tables.xlsx'
+# load the Excel file into a workbook object.
+print(f'[*] Loading {excel_file}.')
 wb = openpyxl.load_workbook(excel_file)
-# Create a dictionary of tables.
+print('[*] Done.')
+# Initialize a dictionary of tables.
 tables = {sheetname: {} for sheetname in wb.sheetnames}
-# Create a dictionary of aliases.
-aliases = {}
-# Create a SQL string.
+# Initialize a SQL string.
 sql = ''
-n = 10
+n = args.rows
+print('[*] Performing inserts.')
 for i in range(n):
+    print(f'[*] On row {i + 1} of {n}...\r', end='')
     sql += f'-- Insert {i+1} of {n}\n'
+    # Initialize a dictionary of aliases.
+    aliases = {}
+    # Generate a random first and last name.
+    first_name = random.choice(first_names)
+    last_name = random.choice(last_names)
     # Iterate through each worksheet and populate the table dictionary.
     for table in list(tables):
         # Get the worksheet.
@@ -90,14 +130,15 @@ for i in range(n):
         for row in ws.iter_rows():
             attribute = row[0].value
             pattern = row[1].value
-            alias = row[2].value
+            alias = row[2].value if len(row) == 3 else ''
             # Make pattern substitutions.
             pattern = replace_lowercase(pattern)
             pattern = replace_uppercase(pattern)
             pattern = replace_digits(pattern)
             pattern = replace_decimals(pattern)
+            pattern = replace_alphanum(pattern)
             pattern = replace_lists(pattern)
-            pattern = replace_names(pattern)
+            pattern = replace_names(pattern, first_name, last_name)
             # Add the alias if one is set.
             if alias and alias not in aliases:
                 aliases[alias] = pattern
@@ -111,9 +152,21 @@ for i in range(n):
         values = [tables[table][field]['pattern'] if not tables[table][field]['alias'] else aliases[tables[table][field]['alias']] for field in fields]
         sql += f'INSERT INTO {table} '
         sql += '(' + ', '.join(fields) + ') '
-        sql += 'VALUES ( ' + ', '.join([f"'{value}'" for value in values]) + ')'
+        sql += 'VALUES (' + ', '.join([f"'{value}'" for value in values]) + ')'
         sql += '\n'
     sql += '\n'
+print('\n[*] Done.')
+print(f'[*] Closing {excel_file}.')
 # Close the workbook.
-# wb.close()
+wb.close()
+print('[*] Done.')
+# Write the SQL to a file.
+sql_file = 'inserts.sql'
+print(f'[*] Writing to {sql_file}')
+with open(sql_file, 'w') as file:
+    file.write(sql)
+print('[*] Done.')
+print('[*] Exiting.')
+
+exit(0)
 
